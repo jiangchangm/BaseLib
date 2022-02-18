7c0e8f82f2854f07cacb63dbee30912c8aeb6f63
jiangchangm <laope_200@163.com>
2022-02-18 16:24:40 +0800

    修改代码

--- example/src/main/java/com/jiangcm/baselib/main/MainActivity.kt
+++ example/src/main/java/com/jiangcm/baselib/main/MainActivity.kt
@@ -26,35 +26,6 @@ class MainActivity : BaseVmActivity<TestViewModel>() {
         mViewModel.refreshProjectList()
     }
 
-    fun getPermission(){
-        PermissionX.init(this)
-            .permissions(
-                Manifest.permission.CAMERA,
-                Manifest.permission.ACCESS_FINE_LOCATION,
-                Manifest.permission.RECORD_AUDIO,
-            )
-            .setDialogTintColor(Color.parseColor("#1972e8"), Color.parseColor("#8ab6f5"))
-            .onExplainRequestReason { scope, deniedList, beforeRequest ->
-                val message = "PermissionX needs following permissions to continue"
-                scope.showRequestReasonDialog(deniedList, message, "Allow", "Deny")
-//                val message = "Please allow the following permissions in settings"
-//                val dialog = CustomDialogFragment(message, deniedList)
-//                scope.showRequestReasonDialog(dialog)
-            }
-            .onForwardToSettings { scope, deniedList ->
-//                val message = "Please allow following permissions in settings"
-//                val dialog = CustomDialogFragment(message, deniedList)
-//                scope.showForwardToSettingsDialog(dialog)
-            }
-            .request { allGranted, grantedList, deniedList ->
-                if (allGranted) {
-                    Toast.makeText(this, "All permissions are granted", Toast.LENGTH_SHORT).show()
-                } else {
-                    Toast.makeText(this, "The following permissions are denied：$deniedList", Toast.LENGTH_SHORT).show()
-                }
-            }
-    }
-
     override fun observe() {
         super.observe()
         mViewModel.retResponse.observe(this) {
