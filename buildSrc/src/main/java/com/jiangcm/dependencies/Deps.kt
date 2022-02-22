96426aee02bc6e328740bff35b3f5e5bbbd6d760
jiangchangm <laope_200@163.com>
2022-02-22 16:13:30 +0800

    java代码更改为kotlin

--- buildSrc/src/main/java/com/jiangcm/dependencies/Deps.kt
+++ buildSrc/src/main/java/com/jiangcm/dependencies/Deps.kt
@@ -21,7 +21,8 @@ object DepsVersion {
     const val jsr305Version = "3.0.2"
     const val leakcanary = "2.8.1"
     const val ereza = "2.3.0"
-    const val lottie="4.2.1"
+    const val lottie = "4.2.1"
+    const val materialDialogs = "3.3.0"
 }
 
 
@@ -29,13 +30,15 @@ object Deps {
     //kotlin
     const val kotlinStdlib =
         "org.jetbrains.kotlin:kotlin-stdlib:${DepsVersion.kotlinVersion}"
-    const val kotlinxCoroutines = "org.jetbrains.kotlinx:kotlinx-coroutines-core:${DepsVersion.kotlinxCoroutinesVersion}"
+    const val kotlinxCoroutines =
+        "org.jetbrains.kotlinx:kotlinx-coroutines-core:${DepsVersion.kotlinxCoroutinesVersion}"
 
     //appcompat
     const val appcompat = "androidx.appcompat:appcompat:${DepsVersion.appcompatSdk}"
     const val activity = "androidx.activity:activity-ktx:${DepsVersion.activityVersion}"
     const val fragment = "androidx.fragment:fragment-ktx:${DepsVersion.fragmentVersion}"
-    const val constraintlayout = "androidx.constraintlayout:constraintlayout:${DepsVersion.constraintlayoutVersion}"
+    const val constraintlayout =
+        "androidx.constraintlayout:constraintlayout:${DepsVersion.constraintlayoutVersion}"
 
     //material
     const val material =
@@ -53,19 +56,25 @@ object Deps {
     const val rxAndroid = "io.reactivex.rxjava2:rxandroid:${DepsVersion.rxAndroidVersion}"
 
     //lifecycle_version
-    const val lifecycleLivedataktx = "androidx.lifecycle:lifecycle-livedata-ktx:${DepsVersion.lifecycleVersion}"
-    const val lifecycleViewmodelKtx = "androidx.lifecycle:lifecycle-viewmodel-ktx:${DepsVersion.lifecycleVersion}"
-    const val lifecycleProcess = "androidx.lifecycle:lifecycle-process:${DepsVersion.lifecycleVersion}"
-    const val lifecycleRuntimeKtx = "androidx.lifecycle:lifecycle-runtime-ktx:${DepsVersion.lifecycleVersion}"
+    const val lifecycleLivedataktx =
+        "androidx.lifecycle:lifecycle-livedata-ktx:${DepsVersion.lifecycleVersion}"
+    const val lifecycleViewmodelKtx =
+        "androidx.lifecycle:lifecycle-viewmodel-ktx:${DepsVersion.lifecycleVersion}"
+    const val lifecycleProcess =
+        "androidx.lifecycle:lifecycle-process:${DepsVersion.lifecycleVersion}"
+    const val lifecycleRuntimeKtx =
+        "androidx.lifecycle:lifecycle-runtime-ktx:${DepsVersion.lifecycleVersion}"
 
-    const val databinding = "androidx.databinding:databinding-runtime:${DepsVersion.dataBindingVersion}"
+    const val databinding =
+        "androidx.databinding:databinding-runtime:${DepsVersion.dataBindingVersion}"
     const val roomRuntime = "androidx.room:room-runtime:${DepsVersion.roomVersion}"
     const val roomKtx = "androidx.room:room-ktx:${DepsVersion.roomVersion}"
 
     //retrofit
     const val retrofit =
         "com.squareup.retrofit2:retrofit:${DepsVersion.retrofitSdkVersion}"
-    const val retrofitConverterGson = "com.squareup.retrofit2:converter-gson:${DepsVersion.retrofitSdkVersion}"
+    const val retrofitConverterGson =
+        "com.squareup.retrofit2:converter-gson:${DepsVersion.retrofitSdkVersion}"
 
     //解析html
     const val jsoup = "org.jsoup:jsoup:${DepsVersion.jsoupVersion}"
@@ -74,12 +83,21 @@ object Deps {
     const val jsr305Annotations = "com.google.code.findbugs:jsr305:${DepsVersion.jsr305Version}"
     const val BaseRecyclerViewAdapterHelper =
         "com.github.CymChad:BaseRecyclerViewAdapterHelper:${DepsVersion.viewHolderVersion}"
+
     //内存泄漏检查
     const val leakCanary = "com.squareup.leakcanary:leakcanary-android:${DepsVersion.leakcanary}"
+
     // 防崩溃
     const val ereza = "cat.ereza:customactivityoncrash:${DepsVersion.ereza}"
+
     //lottie动画
     const val lottie = "com.airbnb.android:lottie:${DepsVersion.lottie}"
+}
 
+object Material {
+    const val materialDialogsCore = "com.afollestad.material-dialogs:core:${DepsVersion.materialDialogs}"
+    const val materialDialogsInput = "com.afollestad.material-dialogs:input:${DepsVersion.materialDialogs}"
+    const val materialDialogsBottomSheets = "com.afollestad.material-dialogs:bottomsheets:${DepsVersion.materialDialogs}"
+    const val materialDialogsLifecycle = "com.afollestad.material-dialogs:lifecycle:${DepsVersion.materialDialogs}"
 }
 
