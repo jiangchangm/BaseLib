c7abd05ae74c303f3a5a840d410f7a6518fe3465
jiangchangm <laope_200@163.com>
2022-03-01 11:13:41 +0800

    修改代码

--- common/src/main/java/com/jiangcm/common/base/BaseActivity.kt
+++ common/src/main/java/com/jiangcm/common/base/BaseActivity.kt
@@ -1,13 +1,26 @@
 package com.jiangcm.common.base
 
+import android.os.Bundle
 import androidx.databinding.ViewDataBinding
 import com.alibaba.android.arouter.launcher.ARouter
 import com.jiangcm.base.base.activity.BaseVmDbActivity
 import com.jiangcm.base.base.vm.BaseViewModel
+import com.jiangcm.common.R
+
 
 abstract class BaseActivity<VM : BaseViewModel, DB : ViewDataBinding> : BaseVmDbActivity<VM, DB>() {
 
+    override fun initView(savedInstanceState: Bundle?) {
+        super.initView(savedInstanceState)
+        overridePendingTransition(R.anim.slide_right_to_left_in, R.anim.slide_right_to_left_out)
+    }
+
     override fun initData() {
         ARouter.getInstance().inject(this)
+    }
+
+    override fun finish() {
+        super.finish()
+        overridePendingTransition(R.anim.slide_left_to_right_in, R.anim.slide_left_to_right_out)
     }
 }
